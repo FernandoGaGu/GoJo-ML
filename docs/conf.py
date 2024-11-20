949894558a8a303b4aead30978dcab3600c83a68
FernandoGaGu <ga.gu.fernando@gmail.com>
2024-11-20 18:56:14 +0100

    Reverted commit

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'gojo - Documentation'
 copyright = '2024, Fernando García Gutiérrez'
 author = 'Fernando García Gutiérrez'
-release = '0.1.5'
+release = '0.1.5.1'
 
 
 # -- General configuration ---------------------------------------------------
